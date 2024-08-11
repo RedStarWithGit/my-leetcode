cb2519da1b5291a361e68158a3474ae5cf9be8f7
RedStar <1261121392@qq.com>
2024-08-11 17:04:07 +0800

    feat: update structure

--- tool/problem_helper.py
+++ tool/problem_helper.py
@@ -10,10 +10,19 @@ from pathlib import Path
 from typing import Optional
 
 
-def create_problem_source_code(problem_id: int, sub_dir: Optional[str] = None):
+def create_problem_source_code(
+        problem_id: int,
+        sub_dir: Optional[str] = 'common',
+        page_size: int = 100
+):
     dir_path = Path(__file__).parent.parent / 'problems'
     if sub_dir:
         dir_path = dir_path / sub_dir
+    if page_size:
+        start = problem_id // page_size * page_size
+        end = start + page_size
+        dir_path = dir_path / f"_{start:0>5}_{end:0>5}"
+
     filepath = str(dir_path / f'{problem_id:0>5}.py')
     if not os.path.exists(filepath):
         os.makedirs(dir_path, exist_ok=True)
